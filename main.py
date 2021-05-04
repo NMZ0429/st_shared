1e4555d735bf88fb51c8d79d92414869b99a0134
NMZ0429 <gen0429@icloud.com>
2021-05-04 22:00:02 +0900

    add resnet

---
+++ main.py
@@ -0,0 +1,86 @@
+import json
+import urllib, urllib.request
+
+import streamlit as st
+import torch
+from PIL import Image
+from torchvision import transforms
+
+from resnet_anime import resnet50
+
+@st.cache
+def load_resnet():
+    # use resnet50
+    model = resnet50()
+    if torch.cuda.is_available():
+        model.to('cuda')
+    model.eval()
+
+    return model
+
+@st.cache
+def preprocess(img):
+    preprocess = transforms.Compose([
+        transforms.Resize(360),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.7137, 0.6628, 0.6519], std=[0.2970, 0.3017, 0.2979]),
+    ])
+
+    batch = preprocess(img).unsqueeze(0)
+
+    if torch.cuda.is_available():
+        batch = batch.to('cuda')
+
+    return batch
+
+def predict_probs(batch, model):
+    with torch.no_grad():
+        output = model(batch)
+        probs = torch.sigmoid(output[0])
+
+    return probs
+
+@st.cache
+def get_label():
+    with urllib.request.urlopen("https://github.com/RF5/danbooru-pretrained/raw/master/config/class_names_6000.json") as url:
+        global class_names
+        class_names = json.loads(url.read().decode())
+
+    return class_names
+
+class_names = get_label()
+
+def calc_result(probs, thresh=0.2):
+    tmp = probs[probs > thresh]
+    inds = probs.argsort(descending=True)
+    txt = ['Predictions with probabilities above ' + str(thresh) + ':\n']
+    for i in inds[0:len(tmp)]:
+        txt +=[ class_names[i] + ': {:.4f} \n'.format(probs[i].cpu().numpy())]
+
+    return txt
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+model = load_resnet()
+
+st.title("Anime Image ResNet")
+
+imgfile = st.file_uploader("Upload Image", type=["png", "jpg"], accept_multiple_files=False)
+
+if imgfile:
+    image = Image.open(imgfile)
+    st.image(
+        image, caption='upload images',
+        use_column_width=True
+    )
+    x = preprocess(image)
+    out = predict_probs(batch=x, model=model)
+    result = calc_result(out)
+
+    st.write(result)
