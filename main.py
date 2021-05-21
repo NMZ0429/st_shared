967fd54b2c237388454ebc31c3210263422ae5f6
NMZ0429 <gen0429@icloud.com>
2021-05-22 05:19:01 +0900

    add labels

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-import urllib, urllib.request
-
+from pandas import read_csv
 import streamlit as st
 import torch
 from PIL import Image
@@ -8,29 +6,38 @@ from torchvision import transforms
 
 from resnet_anime import resnet50
 
+st.set_page_config(page_title="Made by NMZ", layout="wide")
+
+
 @st.cache
 def load_resnet():
     # use resnet50
     model = resnet50()
     if torch.cuda.is_available():
-        model.to('cuda')
+        model.to("cuda")
     model.eval()
 
     return model
 
+
 @st.cache
 def preprocess(img):
-    preprocess = transforms.Compose([
-        transforms.Resize(360),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.7137, 0.6628, 0.6519], std=[0.2970, 0.3017, 0.2979]),
-    ])
+    preprocess = transforms.Compose(
+        [
+            transforms.Resize(360),
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=[0.7137, 0.6628, 0.6519], std=[0.2970, 0.3017, 0.2979]
+            ),
+        ]
+    )
     batch = preprocess(img).unsqueeze(0)
 
     if torch.cuda.is_available():
-        batch = batch.to('cuda')
+        batch = batch.to("cuda")
 
     return batch
+
 
 def predict_probs(batch, model):
     with torch.no_grad():
@@ -39,24 +46,33 @@ def predict_probs(batch, model):
 
     return probs
 
+
 @st.cache
 def get_label():
-    with urllib.request.urlopen("https://github.com/RF5/danbooru-pretrained/raw/master/config/class_names_6000.json") as url:
-        global class_names
-        class_names = json.loads(url.read().decode())
+    column_names = ["_", "en", "ja"]
+    df = read_csv("label_ja.csv", names=column_names)
+    # class_names = json.load(open("label_en.json", "r"))
+    class_names = df.ja.to_list()
 
     return class_names
 
+
 class_names = get_label()
 
-def calc_result(probs, thresh=0.2):
+
+@st.cache
+def calc_result(probs, thresh=0.3):
     tmp = probs[probs > thresh]
     inds = probs.argsort(descending=True)
-    txt = ['Predictions with probabilities above ' + str(thresh) + ':\n']
-    for i in inds[0:len(tmp)]:
-        txt +=[ class_names[i] + ': {:.4f} \n'.format(probs[i].cpu().numpy())]
+    # txt = "## Predictions with probabilities above " + str(thresh) + ":\n"
+    txt = ""
+    for i in inds[0 : len(tmp)]:
+        txt += (
+            "* " + class_names[i] + ": {:.4f} \n".format(probs[i].cpu().numpy()) + "\n"
+        )
 
     return txt
+
 
 hide_streamlit_style = """
             <style>
@@ -69,22 +85,25 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 model = load_resnet()
 
 st.markdown(
-"# 教師無し対照学習でオタクっぽいCNNにする\n\n Image understanding with respect to a specific field (Anime) using Inception trained by SimCLR"
+    "# 教師無し対照学習で汎用オタクCNNを構築する\n\n Image understanding with respect to a specific field (Anime) using Inception trained by SimCLR"
 )
 
-st.markdown("対照学習でアニメ関連の画像をひたすら学習させたらCNNがオタクっぽいことを学習できるんじゃいか？")
+st.markdown("対照学習でアニメ関連の画像をひたすら学習させたらCNNがオタクっぽいことを学習できるか実験")
 
+c1, c2 = st.beta_columns(2)
 
-imgfile = st.file_uploader("Upload Image: (must be at least 360^2)", type=["png", "jpg"], accept_multiple_files=False)
+imgfile = c1.file_uploader(
+    "Upload Image: (must be at least 360^2)",
+    type=["png", "jpg"],
+    accept_multiple_files=False,
+)
 
 if imgfile:
     image = Image.open(imgfile)
-    st.image(
-        image, caption='upload images',
-        use_column_width=True
-    )
+    c1.image(image, caption="upload images", use_column_width=True)
     x = preprocess(image)
     out = predict_probs(batch=x, model=model)
     result = calc_result(out)
 
-    st.write(result)
+    c2.markdown("## 抽出した特徴...(信頼区間>0.3)")
+    c2.markdown(result)
